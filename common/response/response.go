1b2701ab81c24750be90136191b9a89999e7529d
slodkiadrianek <adikurek11@gmail.com>
2026-09-24 09:33:39 +0200

    chore: setup of response helpers

---
+++ common/response/response.go
@@ -0,0 +1,19 @@
+// Package response holds whole logic associated with helper functions
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Send(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if status == http.StatusNoContent {
+		return
+	}
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		panic(err)
+	}
+}
