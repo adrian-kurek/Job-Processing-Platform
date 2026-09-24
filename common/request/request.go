bfd54f2f0aaa7ed7001e52b62811184128411351
slodkiadrianek <adikurek11@gmail.com>
2026-09-24 09:33:25 +0200

    chore: setup of request helpers

---
+++ common/request/request.go
@@ -0,0 +1,80 @@
+// Package request hold whole logic associated with request helpers functions
+package request
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	commonerrors "github.com/adrian-kurek/Job-Processing-Platform/common/errors"
+	"github.com/adrian-kurek/Job-Processing-Platform/common/response"
+)
+
+const (
+	green = "\x1b[32m"
+	reset = "\x1b[0m"
+)
+
+type HTTPFunc func(w http.ResponseWriter, r *http.Request) error
+
+func Make(f HTTPFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		actualDate := time.Now()
+		logTime := actualDate.Format("2006-01-02 15:04:05")
+		if err := f(w, r); err != nil {
+			if apiErr, ok := err.(*commonerrors.API); ok {
+				response.Send(w, apiErr.StatusCode, map[string]string{"message": apiErr.Message})
+			} else {
+				log.Println(err.Error())
+				response.Send(w, http.StatusInternalServerError, map[string]string{"message": "Internal server error"})
+			}
+		}
+		durationOfTheRoute := time.Since(start) / time.Millisecond
+		formattedDurationOfTheRoute := strconv.FormatInt(int64(durationOfTheRoute), 10) + "ms"
+
+		log.Println(green + "[INFO: " + logTime + "] " + r.Method + "-" + r.URL.Path + "-" + r.
+			RemoteAddr + "-" + formattedDurationOfTheRoute + reset)
+	}
+}
+
+func ReadUserIDFromToken(r *http.Request) (int, error) {
+	userID, ok := r.Context().Value("id").(int)
+	if !ok || userID == 0 {
+		err := errors.New("failed to read user from context")
+		return 0, err
+	}
+
+	return userID, nil
+}
+
+func ReadBody[T any](r *http.Request) (*T, error) {
+	if r.Body == nil {
+		return nil, errors.New("no request body provided")
+	}
+	var body T
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	err := decoder.Decode(&body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &body, nil
+}
+
+func ReadQueryParam(r *http.Request, QueryName string) string {
+	name := r.URL.Query().Get(QueryName)
+	return name
+}
+
+func SetContext(r *http.Request, key, data any) *http.Request {
+	ctx := context.WithValue(r.Context(), key, data)
+	return r.WithContext(ctx)
+}
