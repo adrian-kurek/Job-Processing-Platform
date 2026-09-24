76b79edfed904f89a0862ffb4a6daa1eff78d647
slodkiadrianek <adikurek11@gmail.com>
2026-09-24 10:46:20 +0200

    chore: configured logging system for main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,24 +1,8 @@
 package main
 
-import (
-	"io"
-	"log/slog"
-	"os"
-	"time"
-
-	"github.com/DeRuina/timberjack"
-)
+import "github.com/adrian-kurek/Job-Processing-Platform/common/logger"
 
 func main() {
-	logRotator := timberjack.Logger{
-		Filename:         "./logs/app.log",
-		MaxSize:          100,
-		MaxBackups:       7,
-		MaxAge:           30,
-		Compress:         true,
-		RotationInterval: 24 * time.Hour,
-	}
-
-	logger := slog.New(slog.NewJSONHandler(io.MultiWriter(os.Stdout, &logRotator), nil))
+	logger := logger.Setup()
 	logger.Info("Application started")
 }
